11d4b9d154ec9a6042336094019061689662a0e7
Sourav Burman <thesouravburman@gmail.com>
2026-04-19 23:16:32 +0530

    feat: add vehicle categories

---
+++ classifier/categories.py
@@ -0,0 +1,19 @@
+"""
+Vehicle category definitions and label mappings.
+"""
+
+VEHICLE_CATEGORIES = {
+    0: "Two-Wheeler",
+    1: "Sedan",
+    2: "SUV",
+    3: "Bus",
+    4: "Truck",
+}
+
+CATEGORY_COLORS = {
+    "Two-Wheeler": (0,   255, 127),
+    "Sedan":       (30,  144, 255),
+    "SUV":         (255, 165,   0),
+    "Bus":         (220,  20,  60),
+    "Truck":       (148,   0, 211),
+}
